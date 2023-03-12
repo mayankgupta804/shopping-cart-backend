668312803e02ee267fa5c495cc7687d6cc9122a5
mayankgupta804 <mayank.gupta804@gmail.com>
2023-03-12 19:33:32 +0530

    Refactor cart APIs

--- internal/api/cart.go
+++ internal/api/cart.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/hertz-contrib/jwt"
+	"gopkg.in/dealancer/validate.v2"
 )
 
 func NewCartHandler(cartServ service.CartService) *CartHandler {
@@ -25,59 +26,48 @@ func (crtHandler CartHandler) HandleAddToCart(c context.Context, ctx *app.Reques
 	addToCartReq := serializer.AddToCartRequest{}
 	if err = json.Unmarshal(reqData, &addToCartReq); err != nil {
 		fmt.Printf("error encountered while unmarshalling JSON: %v\n", err)
-		ctx.JSON(400, map[string]string{
-			"error": "error encountered while unmarshalling JSON",
-		})
+		ctx.JSON(400, serializer.Error{Error: "error encountered while unmarshalling JSON"})
 		return
 	}
 
-	// if err = validate.Validate(addToCartReq); err != nil {
-	// 	ctx.JSON(400, map[string]string{
-	// 		"error": err.Error(),
-	// 	})
-	// 	return
-	// }
+	if err = validate.Validate(addToCartReq); err != nil {
+		ctx.JSON(400, serializer.Error{Error: err.Error()})
+		return
+	}
 
 	claims := jwt.ExtractClaims(c, ctx)
 	accountID := claims["account_id"].(string)
-
-	// TODO: Remove after testing
-	fmt.Println("accountID: ", accountID)
 
 	if err = crtHandler.cartServ.Add(accountID, addToCartReq); err != nil {
 		// Report issue to sentry and raise an alert
 		fmt.Printf("internal server error: %v\n", err)
-		ctx.JSON(500, map[string]string{
-			"error": err.Error(),
-		})
+		ctx.JSON(500, serializer.Error{Error: err.Error()})
 		return
 	}
 
-	ctx.JSON(200, serializer.AddToCartResponse{Status: "success", Message: "item added successfully"})
+	ctx.JSON(201, serializer.AddToCartResponse{Status: "success", Message: "item added successfully"})
 }
 
 func (crtHandler CartHandler) HandleRemoveFromCart(c context.Context, ctx *app.RequestContext) {
+	var err error
 	itemId, ok := ctx.GetQuery("item_id")
 	if !ok {
-		ctx.JSON(400, map[string]string{
-			"error": "item_id must not be empty.",
-		})
+		ctx.JSON(400, serializer.Error{Error: "item_id must not be empty"})
 		return
 	}
 	removeFromCartReq := serializer.RemoveFromCartRequest{ItemID: itemId}
+	if err = validate.Validate(removeFromCartReq); err != nil {
+		ctx.JSON(400, serializer.Error{Error: err.Error()})
+		return
+	}
 
 	claims := jwt.ExtractClaims(c, ctx)
 	accountID := claims["account_id"].(string)
 
-	// TODO: Remove after testing
-	fmt.Println("accountID: ", accountID)
-
-	if err := crtHandler.cartServ.Remove(accountID, removeFromCartReq); err != nil {
+	if err = crtHandler.cartServ.Remove(accountID, removeFromCartReq); err != nil {
 		// Report issue to sentry and raise an alert
 		fmt.Printf("internal server error: %v\n", err)
-		ctx.JSON(500, map[string]string{
-			"error": err.Error(),
-		})
+		ctx.JSON(500, serializer.Error{Error: err.Error()})
 		return
 	}
 
---
+++ internal/serializer/error.go
@@ -0,0 +1,5 @@
+package serializer
+
+type Error struct {
+	Error string `json:"error"`
+}
