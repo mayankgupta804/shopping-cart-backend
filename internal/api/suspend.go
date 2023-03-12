ddc32918ba816462df32a12505d3c8521e137079
mayankgupta804 <mayank.gupta804@gmail.com>
2023-03-12 19:52:14 +0530

    Update errors in suspend account API

--- internal/api/suspend.go
+++ internal/api/suspend.go
@@ -25,9 +25,7 @@ func (susHandler SuspendHandler) HandleAccountSuspension(c context.Context, ctx 
 	suspendAccountReq := serializer.SuspendAccountRequest{}
 	if err = json.Unmarshal(reqData, &suspendAccountReq); err != nil {
 		fmt.Printf("error encountered while unmarshalling JSON: %v\n", err)
-		ctx.JSON(400, map[string]string{
-			"error": "error encountered while unmarshalling JSON",
-		})
+		ctx.JSON(400, serializer.Error{Error: "JSON body seems to be malformed"})
 		return
 	}
 
@@ -35,25 +33,19 @@ func (susHandler SuspendHandler) HandleAccountSuspension(c context.Context, ctx 
 		// maybe log validation errors to get a hang of how many times
 		// users are unable to register. this would give us the idea
 		// about how we can make the API friendly to use
-		ctx.JSON(400, map[string]string{
-			"error": err.Error(),
-		})
+		ctx.JSON(400, serializer.Error{Error: err.Error()})
 		return
 	}
 
 	err = susHandler.acntService.Suspend(suspendAccountReq)
 
 	if err == service.ErrAccountDoesNotExist {
-		ctx.JSON(400, map[string]string{
-			"error": err.Error(),
-		})
+		ctx.JSON(400, serializer.Error{Error: err.Error()})
 		return
 	} else if err != nil {
 		// Report issue to sentry and raise an alert
 		fmt.Printf("internal server error: %v\n", err)
-		ctx.JSON(500, map[string]string{
-			"error": err.Error(),
-		})
+		ctx.JSON(500, serializer.Error{Error: "internal server error"})
 		return
 	}
 
