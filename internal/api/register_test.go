621e1a1a584b7c27f6e1c2706374d3b822b2e8df
mayankgupta804 <mayank.gupta804@gmail.com>
2023-03-13 12:19:51 +0530

    Add failure test cases for the registration API

--- internal/api/register_test.go
+++ internal/api/register_test.go
@@ -47,3 +47,59 @@ func TestRegisterRequest_Valid(t *testing.T) {
 	assert.DeepEqual(t, "{\"status\":\"success\",\"message\":\"account created successfully.\"}", string(resp.Body()))
 	db.Execute("DELETE FROM accounts WHERE email='hi@hi.com'")
 }
+
+func TestRegisterRequest_Invalid_Request_Body(t *testing.T) {
+	config.Load()
+
+	h := server.Default()
+	databaseCfg := database.Config{
+		Name:     config.App.Database.Name,
+		User:     config.App.Database.User,
+		Password: config.App.Database.Password,
+		Host:     config.App.Database.Host,
+		Port:     config.App.Database.Port,
+	}
+
+	db, err := database.NewFromEnv(context.Background(), &databaseCfg)
+	if err != nil {
+		hlog.Fatal(err)
+	}
+	acntRepo := repository.NewAccountRepository(db)
+	acntService := service.NewAccountService(acntRepo)
+	regnHandler := NewRegistrationHandler(acntService)
+
+	h.POST("/register", regnHandler.HandleRegistration)
+	w := ut.PerformRequest(h.Engine, http.MethodPost, "/register", nil)
+	resp := w.Result()
+	assert.DeepEqual(t, 400, resp.StatusCode())
+	assert.DeepEqual(t, "{\"error\":\"JSON body seems to be malformed\"}", string(resp.Body()))
+}
+
+func TestRegisterRequest_Missing_Request_Body_Field(t *testing.T) {
+	config.Load()
+
+	h := server.Default()
+	databaseCfg := database.Config{
+		Name:     config.App.Database.Name,
+		User:     config.App.Database.User,
+		Password: config.App.Database.Password,
+		Host:     config.App.Database.Host,
+		Port:     config.App.Database.Port,
+	}
+
+	db, err := database.NewFromEnv(context.Background(), &databaseCfg)
+	if err != nil {
+		hlog.Fatal(err)
+	}
+	acntRepo := repository.NewAccountRepository(db)
+	acntService := service.NewAccountService(acntRepo)
+	regnHandler := NewRegistrationHandler(acntService)
+
+	h.POST("/register", regnHandler.HandleRegistration)
+	data := bytes.NewBufferString("{\"name\":\"mayank\", \"role\":\"user\", \"password\":\"hellothere\"}")
+	w := ut.PerformRequest(h.Engine, http.MethodPost, "/register",
+		&ut.Body{Body: data, Len: data.Len()},
+		ut.Header{Key: "Content-Type", Value: "application/json"})
+	resp := w.Result()
+	assert.DeepEqual(t, 400, resp.StatusCode())
+}
